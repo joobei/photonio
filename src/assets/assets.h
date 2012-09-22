e4000bd7b7e0cc0920329f5db46afa36595b4f8f
Nicholas Katzakis <nkatzakis@gmail.com>
2012-09-22 16:49:25 +0900

    mmm

--- src/assets/assets.h
+++ src/assets/assets.h
@@ -10,40 +10,6 @@
 
 namespace pho {
 
-
-    class Mesh {
-	public:
-		Asset();
-		Asset(std::string name);
-		Asset(std::vector<glm::vec3> vertices, std::vector<GLushort> indices, std::vector<glm::vec3> colors, std::string name);
-		Asset(std::vector<glm::vec3> vertices, std::vector<GLushort> indices, std::vector<glm::vec2> texcoords, std::string name);
-		GLuint getVaoId();
-		void render();
-		glm::mat4 modelMatrix;
-		GLuint* shader;
-		GLuint vaoId,iboId,vertexVboId,colorVboId;
-		std::vector<glm::vec3> vertices;
-		std::vector<GLushort> indices;
-		std::vector<glm::vec3> colors;
-		std::vector<glm::vec2> texcoords;
-		GLuint texIndex;
-		GLuint uniformBlockIndex;
-		int numFaces;
-		Box aabb;
-		bool selected;
-		std::string name;
-		int numfaces;
-		glm::vec3 getPosition();
-	};
-
-	struct MyMesh{
-		GLuint vao;
-		GLuint texIndex;
-		GLuint uniformBlockIndex;
-		int numFaces;
-	};
-
-
 	// Shader uniform block stuff *******************
 	struct MyMaterial{
 
