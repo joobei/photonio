571a13b17cecabf999f96077b4830422095a3688
Nicholas Katzakis <nkatzakis@gmail.com>
2012-10-08 19:34:32 +0900

    It's ALIVEcd photoniogitcd photoniogit! - 1st version after refactoring

--- src/assets/mesh.cpp
+++ src/assets/mesh.cpp
@@ -22,7 +22,6 @@ vertices(vertixes),
     CALL_GL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ibId));
 	CALL_GL(glBufferData(GL_ELEMENT_ARRAY_BUFFER,indices.size()*sizeof(GLushort),indices.data(),GL_STATIC_DRAW));
 
-    CALL_GL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ibId));
 	CALL_GL(glBindBuffer(GL_ARRAY_BUFFER,vertexVboId));
 	CALL_GL(glBufferData(GL_ARRAY_BUFFER,vertices.size()*3*sizeof(GLfloat),vertices.data(),GL_STATIC_DRAW));
 	CALL_GL(glVertexAttribPointer(vertexLoc,3,GL_FLOAT,GL_FALSE,0,0));
@@ -32,6 +31,7 @@ vertices(vertixes),
 	CALL_GL(glBufferData(GL_ARRAY_BUFFER,colors.size()*2*sizeof(GLfloat),colors.data(),GL_STATIC_DRAW));
     CALL_GL(glVertexAttribPointer(colorLoc,3,GL_FLOAT,GL_FALSE,0,0));
     CALL_GL(glEnableVertexAttribArray(colorLoc));
+
 	CALL_GL(glBindVertexArray(0));
 
 }
@@ -91,7 +91,8 @@ glm::vec3 pho::Mesh::getPosition() {
 }
 
 void pho::Mesh::draw() {
-    CALL_GL(glBindBuffer(GL_ARRAY_BUFFER,vaoId)); //bind the vao
-    CALL_GL(glDrawElements(GL_TRIANGLES,indices.size(),GL_UNSIGNED_SHORT,&indices[0]));
+    //CALL_GL(glBindBuffer(GL_ARRAY_BUFFER,vaoId)); //bind the vao
+	CALL_GL(glBindVertexArray(vaoId));
+    CALL_GL(glDrawElements(GL_TRIANGLES,indices.size(),GL_UNSIGNED_SHORT,NULL));
 }
 
